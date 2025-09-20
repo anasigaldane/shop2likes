ed4206781327300ae6b73ec1eb15dfb3ef2b2097
Anas Igaldane <Drivetagram@outlook.fr>
2025-09-20 15:51:23 +0200

    Reupload clean project

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 CONFIG = {
-     "ME": "creds_me.json",
-    "IND": "creds_ind.json",
-    "BR": "creds_br.json",
+     "EUROPE": "europe_config.json",
+    "IND": "ind_config.json",
+    "BR": "br_config.json",
+
 }
